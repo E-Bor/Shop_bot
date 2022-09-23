f548d7074aa7a06531c00faf4467e1d774a29c65
E-Bor <zhenya.bormotov@mail.ru>
2022-09-23 20:40:33 +0700

    beta

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 import platform
 
-TOKEN = ""
-
+TOKEN = "5360699267:AAERB5v0pSG9ngWeJX2BlO3gSlZmjskpmxE"
+# TOKEN = "5587049036:AAGKoBH7GlIiv18YKSNeUg74p4CQvr_LvDE"
 
 file_path = os.path.dirname(__file__) + f"\\Products\\files\\" if platform.system() == "Windows" else os.path.dirname(
     __file__) + f"/Products/files/"
